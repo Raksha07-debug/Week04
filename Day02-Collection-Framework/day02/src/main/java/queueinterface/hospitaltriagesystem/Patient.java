661afb235e9542f7188aa6cf56ecf07d0b700955
Raksha <rak070803@gmail.com>
2025-02-13 00:58:55 +0530

    [Raksha] Added: Hospital triage system

---
+++ Day02-Collection-Framework/day02/src/main/java/queueinterface/hospitaltriagesystem/Patient.java
@@ -0,0 +1,18 @@
+package queueinterface.hospitaltriagesystem;
+
+
+public class Patient {
+    String name;
+    public int severity;
+
+    // Constructor to initialize name and severity
+    public Patient(String name, int severity) {
+        this.name = name;
+        this.severity = severity;
+    }
+
+    // Method to return the patient's name
+    public String getName() {
+        return name;
+    }
+}
