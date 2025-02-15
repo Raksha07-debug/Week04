e186f3520191c29a3b63467b778a1f2dda272b2e
Raksha <rak070803@gmail.com>
2025-02-15 12:04:26 +0530

    [Raksha] Added: Inter thread communication

---
+++ Day03-Java-Streams/day03/src/main/java/largefilereader/LargeFileReader.java
@@ -0,0 +1,4 @@
+package largefilereader;
+
+public class LargeFileReader {
+}
---
+++ Day03-Java-Streams/day03/src/test/java/pipedstream/PipedStreamExampleTest.java
@@ -0,0 +1,48 @@
+package readuserinputfromconsole;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.io.*;
+
+class PipedStreamExampleTest {
+    @Test
+    void testPipedStreamCommunication() {
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            Thread writer = new Thread(() -> {
+                try {
+                    pos.write("Test Message".getBytes());
+                    pos.close();
+                } catch (IOException e) {
+                    fail("Writer thread failed: " + e.getMessage());
+                }
+            });
+
+            StringBuilder output = new StringBuilder();
+            Thread reader = new Thread(() -> {
+                try {
+                    int data;
+                    while ((data = pis.read()) != -1) {
+                        output.append((char) data);
+                    }
+                    pis.close();
+                } catch (IOException e) {
+                    fail("Reader thread failed: " + e.getMessage());
+                }
+            });
+
+            writer.start();
+            reader.start();
+
+            writer.join();
+            reader.join();
+
+            assertEquals("Test Message", output.toString());
+
+        } catch (IOException | InterruptedException e) {
+            fail("Test failed: " + e.getMessage());
+        }
+    }
+}
