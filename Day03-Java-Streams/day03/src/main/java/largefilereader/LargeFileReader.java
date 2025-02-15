910b37faa989a066ffb978af5a39aa3961dd8ecf
Raksha <rak070803@gmail.com>
2025-02-15 12:05:28 +0530

    [Raksha] Added: Large file reader

--- Day03-Java-Streams/day03/src/main/java/largefilereader/LargeFileReader.java
+++ Day03-Java-Streams/day03/src/main/java/largefilereader/LargeFileReader.java
@@ -1,4 +1,27 @@
 package largefilereader;
 
+import java.io.*;
+
 public class LargeFileReader {
+    public static String readLargeFile(String filePath) {
+        StringBuilder sb=new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.toLowerCase().contains("error")) {
+                    sb.append(line).append("\n");
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Week04\\Day03-Java-Streams\\day03\\src\\examplefile.txt";
+        readLargeFile(filePath);
+    }
 }
