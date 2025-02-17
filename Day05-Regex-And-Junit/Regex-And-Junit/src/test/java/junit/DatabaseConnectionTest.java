d9cc70eb65b6ccc34c042a7f416a255be26d02c1
Raksha <rak070803@gmail.com>
2025-02-17 13:26:57 +0530

    [Raksha] Added: Testing @BeforeEach and @AfterEach Annotations

---
+++ Day05-Regex-And-Junit/Regex-And-Junit/src/test/java/junit/DatabaseConnectionTest.java
@@ -0,0 +1,30 @@
+package junit;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class DatabaseConnectionTest {
+
+    private DatabaseConnection dbConnection;
+
+    @BeforeEach
+    void setUp() {
+        dbConnection = new DatabaseConnection();
+        dbConnection.connect();
+    }
+
+    @AfterEach
+    void tearDown() {
+        dbConnection.disconnect();
+    }
+
+    @Test
+    void testConnectionIsEstablished() {
+        assertTrue(dbConnection.isConnected());
+    }
+
+    @Test
+    void testConnectionIsClosed() {
+        dbConnection.disconnect();
+        assertFalse(dbConnection.isConnected());
+    }
+}
