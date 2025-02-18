874dcb6331b069453a3e53338fd3455434f06a6f
Raksha <rak070803@gmail.com>
2025-02-18 13:22:29 +0530

    [Raksha] add. dynamic method invocation

---
+++ Day06-Reflection-And-Annotation/Day06-Reflection-And-Annotation/src/main/java/reflection/dynamicmethodinvocation/MathOperation.java
@@ -0,0 +1,17 @@
+package reflection.dynamicmethodinvocation;
+
+public class MathOperation {
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+    }
+
