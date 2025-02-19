342b3d2588d88722e08e89f64f826c3c5c882fad
Raksha <rak070803@gmail.com>
2025-02-19 18:15:22 +0530

    [Raksha] add.  7️⃣ Implement a Custom Caching System with @CacheResult

---
+++ Day06-Reflection-And-Annotation/Day06-Reflection-And-Annotation/src/main/java/annotation/customcachingsystem/ExpensiveService.java
@@ -0,0 +1,15 @@
+package annotation.customcachingsystem;
+
+public class ExpensiveService {
+
+    @CacheResult
+    public int computeSquare(int number) {
+        // Simulate a computationally expensive operation
+        try {
+            Thread.sleep(1000); // Simulate delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return number * number;
+    }
+}
