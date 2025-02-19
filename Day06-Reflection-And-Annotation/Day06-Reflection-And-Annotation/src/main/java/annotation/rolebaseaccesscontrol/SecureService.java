b8c784d7728d4c4384fac3d31c4d856c4274a0d3
Raksha <rak070803@gmail.com>
2025-02-19 15:15:51 +0530

    [Raksha] add.  role based access control

---
+++ Day06-Reflection-And-Annotation/Day06-Reflection-And-Annotation/src/main/java/annotation/rolebaseaccesscontrol/SecureService.java
@@ -0,0 +1,13 @@
+package annotation.rolebaseaccesscontrol;
+public class SecureService {
+
+    @RoleAllowed("ADMIN")
+    public void performAdminTask() {
+        System.out.print("Admin task performed.");
+    }
+
+    @RoleAllowed("USER")
+    public void performUserTask() {
+        System.out.print("User task performed.");
+    }
+}
